8a4febf0842bea92cca93c5edd3263db9d0246fc
PhuongDuy-PP <15520659@gm.uit.edu.vn>
2024-08-29 21:44:27 +0700

    Update CI/CD

--- .github/workflows/build_mysql.sh
+++ .github/workflows/build_mysql.sh
@@ -1,12 +1,7 @@
-# đặt tên cho container muốn kiểm tra
-CONTAINER_NAME=mysql
-
-# -q: chỉ hiển thị ID của container
-# -f -name=^${CONTAINER_NAME}: lọc chính xác tên container
-# kiểm tra xem container có tồn tại hay không
-if [ "(docker ps -a -q -f -name=^${CONTAINER_NAME}$)" ];then
+CONTAINER_NAME=${{secrets.CONTAINER_NAME_SQL}}
+if [ "$(sudo docker ps -a -q -f name=^${CONTAINER_NAME}$)" ];then
     echo "Container $CONTAINER_NAME exists"
 else
     echo "Build container mysql"
-    docker run -d --name $CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=123456 -p 3306:3306 mysql:latest
+    sudo docker run -d --name $CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=123456 -p 3306:3306 mysql:latest
 fi
