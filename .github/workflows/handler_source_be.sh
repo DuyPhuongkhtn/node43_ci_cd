f52ab0fa5ac1d2b80874376eb30ffe132057d128
PhuongDuy-PP <15520659@gm.uit.edu.vn>
2024-08-29 23:58:44 +0700

    Update CI/CD

--- .github/workflows/handler_source_be.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -d "node43_ci_cd" ]; then
-    echo "Folder node43_ci_cd does not exist. Cloning repository..."
-    git clone https://github.com/DuyPhuongkhtn/node43_ci_cd.git
-else
-    echo "Folder node43_ci_cd exists. Pulling latest changes..."
-    cd node43_ci_cd
-    git pull origin main
-fi
