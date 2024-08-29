49a1963d659cc8c6e0a795ec9729ac41b613819d
PhuongDuy-PP <15520659@gm.uit.edu.vn>
2024-08-30 01:00:35 +0700

    Update CI/CD

--- .github/workflows/build_mysql.sh
+++ .github/workflows/build_mysql.sh
@@ -3,5 +3,5 @@ if [ "$(sudo docker ps -a -q -f name=^${CONTAINER_NAME}$)" ];then
     echo "Container $CONTAINER_NAME exists"
 else
     echo "Build container mysql"
-    sudo docker run -d --name $CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=123456 -p 3306:3306 mysql:latest
+    sudo docker run -d --name $CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} -p 3306:3306 mysql:latest
 fi
