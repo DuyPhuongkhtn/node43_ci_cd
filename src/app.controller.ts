4116086ad08d4f50f47057fdd969b1f0b0d4f274
PhuongDuy-PP <15520659@gm.uit.edu.vn>
2024-09-19 19:34:18 +0700

    push new code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,9 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -17,6 +19,6 @@ export class AppController {
 
   @Get("/welcome")
   welcomeNode(): string {
-    return "Welcome NODEJS47"
+    return "Welcome moi nguoi den voi NODEJS47"
   }
 }
